a3d847b9b898831c0e54090445a1d62236c22a5c
Jekoff <jlanloze@gmail.com>
2023-11-04 00:42:27 +0100

    commit

---
+++ project.js
@@ -0,0 +1,23 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on 
+// 3. Collect a bet amount
+// 4. Spin the slot machine 
+// 5. Check  if the user won 
+// 6. gGive the user their winnings
+// 7. Play again
+
+
+const prompt = require("prompt-sync")();
+
+const deposit = () =>{
+    while(true){ 
+    const depositAmount = prompt("Enter a deposit amount: ")
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+        console.log("Invalid deposit amount, try again");
+    }
+ }
+}
+
+deposit();
