4d8d4bf1bfbb5d717ef91372e94f68246f99231c
Jekoff <jlanloze@gmail.com>
2023-11-05 00:35:56 +0100

    Collect a bet amount

--- project.js
+++ project.js
@@ -22,5 +22,32 @@ const deposit = () =>{
  }
 }
 
-const depositAmount = deposit();
-console.log(depositAmount);
+const getNumberOfLines = () => {
+    while(true){ 
+        const lines = prompt("Enter the number of lines to bet on (1-3): ")
+        const numberOfLines = parseFloat(lines);
+    
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Invalid number of lines, try again");
+        }else{
+            return numberOfLines;
+        }
+     }
+}
+
+const getBet = (balance, lines) => {
+    while(true){ 
+        const bet = prompt("Enter the bet per line: ")
+        const numberBet = parseFloat(bet);
+    
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again.");
+        }else{
+            return numberBet;
+        }
+     }
+};
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance);
