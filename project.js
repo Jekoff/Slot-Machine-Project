ae156c4b3b2849e1721b616d6a939570d6b17624
Jekoff <jlanloze@gmail.com>
2023-11-05 00:56:40 +0100

    commit

--- project.js
+++ project.js
@@ -9,6 +9,24 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8 
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
 const deposit = () =>{
     while(true){ 
     const depositAmount = prompt("Enter a deposit amount: ")
@@ -47,6 +65,10 @@ const getBet = (balance, lines) => {
         }
      }
 };
+
+const spin = () => {
+    const symbols = [];
+}
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
