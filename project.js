c26b00d88bcc9a32f5a4e9dc5e1631bd7f91acb0
Jérôme Lanloze <99019668+Jekoff@users.noreply.github.com>
2023-11-05 22:41:13 +0100

    Spin the slot machine

--- project.js
+++ project.js
@@ -13,17 +13,17 @@ const ROWS = 3;
 const COLS = 3;
 
 const SYMBOLS_COUNT = {
-    "A": 2,
-    "B": 4,
-    "C": 6,
-    "D": 8 
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8 
 }
 
 const SYMBOL_VALUES = {
-    "A": 5,
-    "B": 4,
-    "C": 3,
-    "D": 2
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
 }
 
 
@@ -68,7 +68,25 @@ const getBet = (balance, lines) => {
 
 const spin = () => {
     const symbols = [];
+    for(const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+        for(let i = 0; i < count; i++){
+            symbols.push(symbol); 
+        }
+    }
+    const reels = [[], [], []];
+    for(let i = 0; i < COLS; i++){
+        const reelSymbols = [...symbols];
+        for(let j = 0; j < ROWS; j++){
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+        const selectedSymbol = reelSymbols[randomIndex];
+        reels[i].push(selectedSymbol);
+        reelSymbols.splice(randomIndex, 1);
+        }
+    }  
+    return reels;
 }
+const reels = spin();
+console.log(reels);
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
